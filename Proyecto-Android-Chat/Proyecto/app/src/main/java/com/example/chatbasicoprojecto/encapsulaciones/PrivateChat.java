8143915711bca587c6ab426c6003f282ef5540bf
randaegs <garciasanchezrd@gmail.com>
2024-09-24 17:58:41 -0400

    Crear clase de mensaje y anadirla a chat privado

--- Proyecto-Android-Chat/Proyecto/app/src/main/java/com/example/chatbasicoprojecto/encapsulaciones/PrivateChat.java
+++ Proyecto-Android-Chat/Proyecto/app/src/main/java/com/example/chatbasicoprojecto/encapsulaciones/PrivateChat.java
@@ -1,7 +1,43 @@
 package com.example.chatbasicoprojecto.encapsulaciones;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrivateChat {
-    private String userUUID;
-    private String contactUUID;
-    private String messages;
+    private String ownerUsername;
+    private String contactUsername;
+    private List<Message> messageList;
+
+    public PrivateChat() {
+    }
+
+    public PrivateChat(String ownerUsername, String contactUsername) {
+        this.ownerUsername = ownerUsername;
+        this.contactUsername = contactUsername;
+        this.messageList = new ArrayList<>();
+    }
+
+    public String getOwnerUsername() {
+        return ownerUsername;
+    }
+
+    public void setOwnerUsername(String ownerUsername) {
+        this.ownerUsername = ownerUsername;
+    }
+
+    public String getContactUsername() {
+        return contactUsername;
+    }
+
+    public void setContactUsername(String contactUsername) {
+        this.contactUsername = contactUsername;
+    }
+
+    public List<Message> getMessageList() {
+        return messageList;
+    }
+
+    public void setMessageList(List<Message> messageList) {
+        this.messageList = messageList;
+    }
 }
