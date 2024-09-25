26f1f839bb3d7b53a69e51f2905880395c745150
randaegs <garciasanchezrd@gmail.com>
2024-09-24 23:35:36 -0400

    Crear messageitem recycler adapter para mostrar mensajes de usuarios en chat

--- Proyecto-Android-Chat/Proyecto/app/src/main/java/com/example/chatbasicoprojecto/PrivateChatActivity.java
+++ Proyecto-Android-Chat/Proyecto/app/src/main/java/com/example/chatbasicoprojecto/PrivateChatActivity.java
@@ -10,12 +10,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.chatbasicoprojecto.adapters.MessageItemRecyclerAdapter;
 import com.example.chatbasicoprojecto.databinding.ActivityLoginBinding;
 import com.example.chatbasicoprojecto.databinding.ActivityPrivateChatBinding;
 
 public class PrivateChatActivity extends AppCompatActivity {
     private ActivityPrivateChatBinding binding;
+    private RecyclerView recyclerView;
+    private MessageItemRecyclerAdapter messageItemRecyclerAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,5 +28,12 @@ public class PrivateChatActivity extends AppCompatActivity {
 
         binding = ActivityPrivateChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+
+        recyclerView = findViewById(R.id.messages_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        messageItemRecyclerAdapter = new MessageItemRecyclerAdapter(intent.getStringExtra("username"), intent.getStringExtra("contactUsername"));
+        recyclerView.setAdapter(messageItemRecyclerAdapter);
     }
 }
