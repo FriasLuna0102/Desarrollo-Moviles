e506074bd515990c4f2f63ecd09295c2f752f496
randaegs <garciasanchezrd@gmail.com>
2024-09-25 10:40:55 -0400

    Obtener contenido de mensaje de textview

--- Proyecto-Android-Chat/Proyecto/app/src/main/java/com/example/chatbasicoprojecto/PrivateChatActivity.java
+++ Proyecto-Android-Chat/Proyecto/app/src/main/java/com/example/chatbasicoprojecto/PrivateChatActivity.java
@@ -3,6 +3,8 @@ package com.example.chatbasicoprojecto;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -16,11 +18,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.chatbasicoprojecto.adapters.MessageItemRecyclerAdapter;
 import com.example.chatbasicoprojecto.databinding.ActivityLoginBinding;
 import com.example.chatbasicoprojecto.databinding.ActivityPrivateChatBinding;
+import com.google.firebase.database.DatabaseReference;
 
 public class PrivateChatActivity extends AppCompatActivity {
     private ActivityPrivateChatBinding binding;
     private RecyclerView recyclerView;
     private MessageItemRecyclerAdapter messageItemRecyclerAdapter;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,5 +39,11 @@ public class PrivateChatActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         messageItemRecyclerAdapter = new MessageItemRecyclerAdapter(intent.getStringExtra("username"), intent.getStringExtra("contactUsername"));
         recyclerView.setAdapter(messageItemRecyclerAdapter);
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.message_content);
+        String text = editText.getText().toString();
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
