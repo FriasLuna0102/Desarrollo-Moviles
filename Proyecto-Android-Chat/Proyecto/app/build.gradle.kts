d47956fa9173713285053ea5429f48a2baaa4545
friasluna01 <sfrias@clickping.do>
2024-09-28 12:57:53 -0400

    chore(build.gradle):  add the dependency of the firabase storage and the glide for read the urls of the images.

--- Proyecto-Android-Chat/Proyecto/app/build.gradle.kts
+++ Proyecto-Android-Chat/Proyecto/app/build.gradle.kts
@@ -44,7 +44,9 @@ dependencies {
     // Add the dependency for the Realtime Database library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation(libs.firebase.database)
-
+    implementation("com.google.firebase:firebase-storage")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
